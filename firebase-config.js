a83be04446714c71821786c64a201fc28351f870
Eddie’s World <keschflow@gmail.com>
2026-05-16 17:04:31 +0200

    Add Firebase config for RAW LOG archive

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0olKESyTP0rXUlnjGstLlGN50I1m_O2A",
+  authDomain: "mg-challenge.firebaseapp.com",
+  projectId: "mg-challenge",
+  storageBucket: "mg-challenge.firebasestorage.app",
+  messagingSenderId: "472289048663",
+  appId: "1:472289048663:web:cc68ded6007e1e822c4b12"
+};
